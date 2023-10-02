4976a43d233a6923045d0515504230a09f65fdaf
SeideineKatze <thongprasert_k3@su.ac.th>
2023-10-02 15:29:29 +0700

    2/10/2566

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'home.html')
 
 def second(request):
     return render(request, 'login.html')
+
+def blog(request):
+    return render(request, 'blog1.html')
